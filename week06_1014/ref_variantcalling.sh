425d65656c6945a35213348f4c6dbd074ef21f49
r15455017 <r15455017@ntu.edu.tw>
2026-09-24 20:17:05 +0800

    Fix module name casing for Biology

--- week06_1014/ref_variantcalling.sh
+++ week06_1014/ref_variantcalling.sh
@@ -67,7 +67,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S')"
 
 # set up the environment for variant calling
 # GATK_PATH=/opt/ohpc/Taiwania3/pkg/biology/GATK/gatk_v4.2.3.0
-module load Biology
+module load biology
 module load Python
 module load GATK/4.2.3.0
 
